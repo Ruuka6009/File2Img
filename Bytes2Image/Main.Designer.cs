bb61ea75379066b10cbab29c632c5e4c6d076389
Ruuka <128378374+Ruuka6009@users.noreply.github.com>
2023-05-08 23:21:04 +0200

    Initial commit
    🍝 code.

---
+++ Bytes2Image/Main.Designer.cs
@@ -0,0 +1,215 @@
+﻿namespace Bytes2Image
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.liveView = new System.Windows.Forms.PictureBox();
+            this.selBtn = new System.Windows.Forms.Button();
+            this.pathTxt = new System.Windows.Forms.TextBox();
+            this.fileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.rwbytes = new System.Windows.Forms.Button();
+            this.pbRBytes = new System.Windows.Forms.ProgressBar();
+            this.currentProgressLabel = new System.Windows.Forms.Label();
+            this.imgsize = new System.Windows.Forms.Label();
+            this.coordsLabel = new System.Windows.Forms.Label();
+            this.basePixels = new System.Windows.Forms.Label();
+            this.maxPixels = new System.Windows.Forms.Label();
+            this.nullPixels = new System.Windows.Forms.Label();
+            this.livePreview = new System.Windows.Forms.CheckBox();
+            this.switchRW = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.liveView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // liveView
+            // 
+            this.liveView.Image = global::Bytes2Image.Properties.Resources.empty_folder;
+            this.liveView.Location = new System.Drawing.Point(412, 12);
+            this.liveView.Name = "liveView";
+            this.liveView.Size = new System.Drawing.Size(150, 150);
+            this.liveView.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.liveView.TabIndex = 0;
+            this.liveView.TabStop = false;
+            // 
+            // selBtn
+            // 
+            this.selBtn.Location = new System.Drawing.Point(331, 12);
+            this.selBtn.Name = "selBtn";
+            this.selBtn.Size = new System.Drawing.Size(75, 23);
+            this.selBtn.TabIndex = 1;
+            this.selBtn.Text = "Select";
+            this.selBtn.UseVisualStyleBackColor = true;
+            this.selBtn.Click += new System.EventHandler(this.selBtn_Click);
+            // 
+            // pathTxt
+            // 
+            this.pathTxt.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pathTxt.Location = new System.Drawing.Point(12, 12);
+            this.pathTxt.Name = "pathTxt";
+            this.pathTxt.ReadOnly = true;
+            this.pathTxt.Size = new System.Drawing.Size(313, 23);
+            this.pathTxt.TabIndex = 2;
+            this.pathTxt.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.pathTxt.Click += new System.EventHandler(this.pathTxt_Click);
+            // 
+            // rwbytes
+            // 
+            this.rwbytes.Location = new System.Drawing.Point(331, 41);
+            this.rwbytes.Name = "rwbytes";
+            this.rwbytes.Size = new System.Drawing.Size(52, 23);
+            this.rwbytes.TabIndex = 3;
+            this.rwbytes.Text = "Read";
+            this.rwbytes.UseVisualStyleBackColor = true;
+            this.rwbytes.Click += new System.EventHandler(this.rbytes_Click);
+            // 
+            // pbRBytes
+            // 
+            this.pbRBytes.Location = new System.Drawing.Point(12, 41);
+            this.pbRBytes.Name = "pbRBytes";
+            this.pbRBytes.Size = new System.Drawing.Size(275, 23);
+            this.pbRBytes.TabIndex = 4;
+            // 
+            // currentProgressLabel
+            // 
+            this.currentProgressLabel.AutoSize = true;
+            this.currentProgressLabel.Location = new System.Drawing.Point(293, 45);
+            this.currentProgressLabel.Name = "currentProgressLabel";
+            this.currentProgressLabel.Size = new System.Drawing.Size(32, 15);
+            this.currentProgressLabel.TabIndex = 5;
+            this.currentProgressLabel.Text = "---%";
+            // 
+            // imgsize
+            // 
+            this.imgsize.AutoSize = true;
+            this.imgsize.Location = new System.Drawing.Point(12, 67);
+            this.imgsize.Name = "imgsize";
+            this.imgsize.Size = new System.Drawing.Size(59, 15);
+            this.imgsize.TabIndex = 6;
+            this.imgsize.Text = "???? x ????";
+            // 
+            // coordsLabel
+            // 
+            this.coordsLabel.AutoSize = true;
+            this.coordsLabel.Location = new System.Drawing.Point(12, 82);
+            this.coordsLabel.Name = "coordsLabel";
+            this.coordsLabel.Size = new System.Drawing.Size(50, 15);
+            this.coordsLabel.TabIndex = 7;
+            this.coordsLabel.Text = "????;????";
+            // 
+            // basePixels
+            // 
+            this.basePixels.AutoSize = true;
+            this.basePixels.Location = new System.Drawing.Point(98, 67);
+            this.basePixels.Name = "basePixels";
+            this.basePixels.Size = new System.Drawing.Size(42, 15);
+            this.basePixels.TabIndex = 8;
+            this.basePixels.Text = "???????";
+            // 
+            // maxPixels
+            // 
+            this.maxPixels.AutoSize = true;
+            this.maxPixels.Location = new System.Drawing.Point(98, 82);
+            this.maxPixels.Name = "maxPixels";
+            this.maxPixels.Size = new System.Drawing.Size(42, 15);
+            this.maxPixels.TabIndex = 9;
+            this.maxPixels.Text = "???????";
+            // 
+            // nullPixels
+            // 
+            this.nullPixels.AutoSize = true;
+            this.nullPixels.Location = new System.Drawing.Point(187, 75);
+            this.nullPixels.Name = "nullPixels";
+            this.nullPixels.Size = new System.Drawing.Size(17, 15);
+            this.nullPixels.TabIndex = 10;
+            this.nullPixels.Text = "??";
+            // 
+            // livePreview
+            // 
+            this.livePreview.AutoSize = true;
+            this.livePreview.Location = new System.Drawing.Point(315, 71);
+            this.livePreview.Name = "livePreview";
+            this.livePreview.Size = new System.Drawing.Size(91, 19);
+            this.livePreview.TabIndex = 11;
+            this.livePreview.Text = "Live preview";
+            this.livePreview.UseVisualStyleBackColor = true;
+            // 
+            // switchRW
+            // 
+            this.switchRW.Location = new System.Drawing.Point(383, 41);
+            this.switchRW.Name = "switchRW";
+            this.switchRW.Size = new System.Drawing.Size(23, 23);
+            this.switchRW.TabIndex = 12;
+            this.switchRW.Text = "🔀";
+            this.switchRW.UseVisualStyleBackColor = true;
+            this.switchRW.Click += new System.EventHandler(this.switchRW_Click);
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(573, 170);
+            this.Controls.Add(this.switchRW);
+            this.Controls.Add(this.livePreview);
+            this.Controls.Add(this.nullPixels);
+            this.Controls.Add(this.maxPixels);
+            this.Controls.Add(this.basePixels);
+            this.Controls.Add(this.coordsLabel);
+            this.Controls.Add(this.imgsize);
+            this.Controls.Add(this.currentProgressLabel);
+            this.Controls.Add(this.pbRBytes);
+            this.Controls.Add(this.rwbytes);
+            this.Controls.Add(this.pathTxt);
+            this.Controls.Add(this.selBtn);
+            this.Controls.Add(this.liveView);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Main";
+            this.Text = "Main";
+            this.Load += new System.EventHandler(this.Main_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.liveView)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private PictureBox liveView;
+        private Button selBtn;
+        private TextBox pathTxt;
+        private OpenFileDialog fileDialog;
+        private Button rwbytes;
+        private ProgressBar pbRBytes;
+        private Label currentProgressLabel;
+        private Label imgsize;
+        private Label coordsLabel;
+        private Label maxPixels;
+        private Label basePixels;
+        private Label nullPixels;
+        private CheckBox livePreview;
+        private Button switchRW;
+    }
+}
---
+++ Bytes2Image/Main.cs
@@ -0,0 +1,253 @@
+﻿using System.Diagnostics;
+using System.Drawing.Imaging;
+
+namespace Bytes2Image
+{
+    public partial class Main : Form
+    {
+        Settings settings = new Settings();
+
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+        private void Main_Load(object sender, EventArgs e)
+        {
+            settings.DesktopPath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+            settings.CanRead = false;
+            settings.imgSize = 0;
+            settings.AllBytes = 0;
+            settings.FilePath = String.Empty;
+            settings.Save();
+            refreshUI();
+        }
+
+        private void selBtn_Click(object sender, EventArgs e)
+        {
+            selectPath();
+        }
+
+        private void pathTxt_Click(object sender, EventArgs e)
+        {
+            selectPath();
+        }
+
+        private void selectPath()
+        {
+            fileDialog.InitialDirectory = settings.DesktopPath;
+            fileDialog.Filter = "All files (*.*)|*.*";
+            if (fileDialog.ShowDialog() != DialogResult.OK) return;
+            if (!File.Exists(fileDialog.FileName))
+            {
+                MessageBox.Show("Can't open file.", "⚠ Error.");
+                return;
+            }
+            settings.FilePath = fileDialog.FileName;
+
+            FileInfo fileInfo = new FileInfo(settings.FilePath);
+            Debug.WriteLine(fileInfo.Length);
+
+            if (fileInfo.Length >= Convert.ToInt64(2147483648))
+            {
+                MessageBox.Show("File is more than 2GB!", "⚠ Error.");
+                return;
+            }
+
+            pathTxt.Text = settings.FilePath;
+            settings.AllBytes = fileInfo.Length;
+            settings.imgSize = (int)Math.Sqrt(settings.AllBytes) + 2;
+            settings.CanRead = true;
+
+            settings.Save();
+
+            refreshUI();
+        }
+        private void refreshUI()
+        {
+            if (settings.CanRead) rwbytes.Enabled = true;
+            if (!settings.CanRead) rwbytes.Enabled = false;
+
+            if(settings.rw) rwbytes.Text = "Read";
+            if (!settings.rw) rwbytes.Text = "Write";
+        }
+
+        private void ImageToFile()
+        {
+            try
+            {
+                using (Image image = Image.FromFile(settings.FilePath))
+                {
+                    Bitmap bitmap = new Bitmap(image);
+                    int imgSize = (bitmap.Height * bitmap.Width);
+                    int imgX = 0;
+                    int imgY = 0;
+                    int percentage = 0;
+                    int curbyte = 0;
+
+                    int oldX = 0;
+                    int oldY = 0;
+
+                    bool running = true;
+
+                    int height = bitmap.Height - 1;
+
+                    Color currPixel = Color.Red;
+                    Color oldPixel = Color.Green;
+
+                    liveView.Image = bitmap;
+
+                    List<byte> b = new List<byte>();
+                    setSizeTxt(bitmap.Height + " x " + bitmap.Width, imgSize.ToString(), "???", "???");
+
+                    while(running)
+                    {
+                        currPixel = bitmap.GetPixel(imgX, imgY);
+
+                        if (livePreview.Checked)
+                        {
+                            setCoordsTxt(imgX.ToString() + ";" + imgY.ToString());
+                            bitmap.SetPixel(imgX, imgY, Color.FromArgb(255, percentage * 2, 0, 0));
+                            liveView.Image = bitmap;
+                        }
+
+                        if (currPixel.A < 255) running = false;
+
+                        b.Add(currPixel.R);
+                        percentage = (int)((double)curbyte / imgSize * 100);
+                        setPercentage(percentage.ToString() + "%", percentage);
+
+                        curbyte++;
+
+                        if (imgX < height)
+                        {
+                            imgX++;
+                        }
+                        else
+                        {
+                            imgX = 0;
+                            imgY++;
+                        }
+                    }
+                    percentage = 100;
+                    setPercentage(percentage.ToString() + "%", percentage);
+                    byte[] bArray = b.ToArray();
+
+                    string tempstr = Path.GetFileName(settings.FilePath).ToString();
+
+                    File.WriteAllBytes(settings.FilePath.Replace((Path.GetFileName(settings.FilePath)), "") + @"\ " + tempstr.Substring(0, tempstr.Length-4), bArray);
+                }
+            }
+            catch (OutOfMemoryException ex)
+            {
+                MessageBox.Show("Not a valid image!", "⚠ Error.");
+                return;
+            }
+        }
+
+        private byte[] AddByteToArray(byte[] bArray, byte newByte)
+        {
+            byte[] newArray = new byte[bArray.Length + 1];
+            bArray.CopyTo(newArray, 1);
+            newArray[0] = newByte;
+            return newArray;
+        }
+
+        private void FileToImage()
+        {
+            pbRBytes.Maximum = 100;
+            pbRBytes.Minimum = 0;
+
+            long curbyte = 0;
+            int percentage = 0;
+            var bitmap = new Bitmap(settings.imgSize, settings.imgSize);
+
+            int imgX = 0;
+            int imgY = 0;
+
+            int imgSize = (settings.imgSize * settings.imgSize);
+
+            setSizeTxt(settings.imgSize.ToString() + " x " + settings.imgSize.ToString(), imgSize.ToString(), settings.AllBytes.ToString(), (imgSize - settings.AllBytes).ToString());
+
+            liveView.Image = bitmap;
+
+            using (StreamWriter writer = new StreamWriter(settings.BytesFile))
+            {
+                byte[] readFile = File.ReadAllBytes(settings.FilePath);
+                foreach (byte b in readFile)
+                {
+                    bitmap.SetPixel(imgX, imgY, Color.FromArgb(255, b, b, b));
+
+                    if (livePreview.Checked)
+                    {
+                        liveView.Image = bitmap;
+                        setCoordsTxt(imgX.ToString() + ";" + imgY.ToString());
+                    }
+                    curbyte++;
+                    //writer.Write(b);
+                    if (curbyte < settings.AllBytes) writer.Write(",");
+
+                    if (imgX < settings.imgSize - 1)
+                    {
+                        imgX++;
+                    }
+                    else
+                    {
+                        imgX = 0;
+                        imgY++;
+                    }
+
+                    percentage = (int)((double)curbyte / settings.AllBytes * 100);
+                    setPercentage(percentage.ToString() + "%", percentage);
+                }
+                bitmap.Save(settings.FilePath.Replace((Path.GetFileName(settings.FilePath)), "") + @"\" + Path.GetFileName(settings.FilePath).ToString() + ".png", ImageFormat.Png);
+                liveView.Image = Image.FromFile(settings.FilePath.Replace((Path.GetFileName(settings.FilePath)), "") + @"\" + Path.GetFileName(settings.FilePath).ToString() + ".png");
+
+                bitmap.Dispose();
+            }
+        }
+
+        private void setImage(Image img)
+        {
+            this.Invoke(() => liveView.Image = img);
+        }
+        private void setPercentage(string txt, int percentage)
+        {
+            this.Invoke(() => pbRBytes.Value = percentage);
+            this.Invoke(() => currentProgressLabel.Text = txt);
+        }
+        private void setSizeTxt(string txt, string bPixelsTxt, string mPixelsTxt, string nPixelsTxt)
+        {
+            this.Invoke(() => imgsize.Text = txt);
+            this.Invoke(() => basePixels.Text = bPixelsTxt);
+            this.Invoke(() => maxPixels.Text = mPixelsTxt);
+            this.Invoke(() => nullPixels.Text = nPixelsTxt);
+        }
+        private void setCoordsTxt(string txt)
+        {
+            this.Invoke(() => coordsLabel.Text = txt);
+        }
+
+        private void rbytes_Click(object sender, EventArgs e)
+        {
+            if (!settings.CanRead) return;
+            if(settings.rw)
+            {
+                Thread readBytesThread = new Thread(new ThreadStart(FileToImage));
+                readBytesThread.Start();
+            } 
+            else
+            {
+                Thread writeBytesThread = new Thread(new ThreadStart(ImageToFile));
+                writeBytesThread.Start();
+            }
+        }
+
+        private void switchRW_Click(object sender, EventArgs e)
+        {
+            settings.rw = !settings.rw;
+            settings.Save();
+            refreshUI();
+        }
+    }
+}
